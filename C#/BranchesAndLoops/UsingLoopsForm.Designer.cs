4553a981bd7f94751c26dcdd9065fc602e606d73
Madison Mehalko <mmehalko@spscc.edu>
2019-05-21 16:35:06 -0700

    Added C# folder with Branches and Loops program

---
+++ C#/BranchesAndLoops/UsingLoopsForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace BranchesAndLoops
+{
+    partial class UsingLoopsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLoop = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStep = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Type of &Loop:";
+            // 
+            // txtLoop
+            // 
+            this.txtLoop.Location = new System.Drawing.Point(140, 12);
+            this.txtLoop.Name = "txtLoop";
+            this.txtLoop.Size = new System.Drawing.Size(100, 26);
+            this.txtLoop.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&Start Value";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(140, 67);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 26);
+            this.txtStart.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "&End Value:";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(140, 122);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 26);
+            this.txtEnd.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "S&tep Value:";
+            // 
+            // txtStep
+            // 
+            this.txtStep.Location = new System.Drawing.Point(140, 177);
+            this.txtStep.Name = "txtStep";
+            this.txtStep.Size = new System.Drawing.Size(100, 26);
+            this.txtStep.TabIndex = 7;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 237);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(51, 20);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "label5";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(310, 15);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(128, 48);
+            this.btnProcess.TabIndex = 9;
+            this.btnProcess.Text = "&Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(310, 69);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(128, 48);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(310, 123);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(128, 48);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // UsingLoopsForm
+            // 
+            this.AcceptButton = this.btnProcess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(453, 283);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtStep);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLoop);
+            this.Controls.Add(this.label1);
+            this.Name = "UsingLoopsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Using Branches and Loops";
+            this.Load += new System.EventHandler(this.UsingLoopsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLoop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStep;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ C#/BranchesAndLoops/UsingLoopsForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BranchesAndLoops
+{
+    public partial class UsingLoopsForm : Form
+    {
+        public UsingLoopsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            // execute specified loop if entries are valid
+            // variable for errors;
+            string errorMessage;
+            // are entries valid?
+            // is type of loop for or while?
+            string loopType = txtLoop.Text.ToLower(); // reads loop as lower case entries and assign to variable.
+            switch (loopType)
+            {
+                case "for": // if the value in the text box = for or while
+                case "while":
+                    errorMessage = ""; // variable is an empty string, display if above qualifications are met.
+                    break; // if the value does not match the above qualifications, go on to the next option.
+                default:
+                    errorMessage = "Loop type not found. "; // If variables are not met, message is read out.
+                    break;
+            }
+
+            // is start value a decimal > 0?
+            decimal startValue; // variable for txtStart.
+            if (decimal.TryParse(txtStart.Text, out startValue)) // parse txtStart into a decimal and read it out as your above variable (startValue)
+            {
+                if (startValue <= 0) // is entered number above zero?
+                    errorMessage += "Start value needs to be more than zero."; // User gave a number that can't be used. Error displayed.
+            }
+            else
+                errorMessage += "Start value needs to be a decimal more than zero."; // if number isn't entered, error message is read out.
+
+            // is end value a decimal > 0?
+            decimal endValue; // variable for txtEnd.
+            if (decimal.TryParse(txtEnd.Text, out endValue)) // parse txtEnd into a decimal and read it out as above variable (endValue).
+            {
+                // If entered endValue (from txtEnd) reads a number but less than zero, show error message.
+                if (endValue <= 0) 
+                    errorMessage += "End value needs to be more than zero."; 
+            }
+            else
+                // If entered text isn't a number, show error message.
+                errorMessage += "End value needs to be a decimal more than zero.";
+
+            // is step value a decimal > 0?
+            decimal stepValue; // variable for txtStep
+            if (decimal.TryParse(txtStep.Text, out stepValue)) // parse txtStep into a deceimal and read it out as above variable.
+            {
+                if (stepValue <= 0)
+                    errorMessage += "Step value needs to be more than zero.";
+            }
+            else
+                errorMessage += "Step value needs to be a decimal more than zero.";
+
+            // if all entries ok, is start < end?
+            if (errorMessage == "")
+            {
+                // is start < end?
+                if (startValue < endValue)
+                {
+                    // run loop
+                    // clear display label
+                    lblStatus.Text = string.Empty;
+
+                    // determine kind of loop
+                    switch(loopType)
+                    {
+                        case "for": // if loop type is 'for'
+                            for (decimal counter = startValue ; // counter is a decimal, start at startValue 
+                            counter <= endValue; counter += stepValue) // if counter is less than or equal to endValue, increase by 1.
+                            // once our counter hits our end value, the loop stops at the break and continues
+                            {
+                                lblStatus.Text += ".";
+                            }
+                            break;
+                        /*case "while": // if loop type is 'while'
+                            break;*/
+                        default: // acts like an 'else' statement. If loop type is not 'for', display information below.
+                            while (startValue<=endValue)
+                            {
+                                startValue += stepValue;
+                                lblStatus.Text += ".";
+                            }
+                            break;
+                    }
+
+                }
+                else
+                    errorMessage = "Start value must be less than end value.";
+            }
+            if (errorMessage != "") // If there is not equal to an empty string...
+                lblStatus.Text = errorMessage; // Display your errorMessage in your display label.
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // clear form for new entry
+            txtLoop.Clear();
+            txtStart.Clear();
+            txtEnd.Clear();
+            txtStep.Clear();
+            lblStatus.Text = string.Empty;
+            txtLoop.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // close and exit
+            Close();
+        }
+
+        private void UsingLoopsForm_Load(object sender, EventArgs e)
+        {
+            // clear display label
+            lblStatus.Text = string.Empty;
+        }
+    }
+}
